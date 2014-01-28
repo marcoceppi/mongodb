ee0db12b9e0b34d9f5b8e59df0349063cc951d40
Charles Butler <chuck@dasroot.net>
2014-01-28 16:51:49 -0500

    Added missing apt-get update

--- tests/00_setup.sh
+++ tests/00_setup.sh
@@ -6,4 +6,6 @@ if [ -f '/etc/apt.d/sources.list.d/juju-stable-precise.list' ]; then
 	sudo add-apt-repository ppa:juju/stable -y
 fi
 
+sudo apt-get update
+
 sudo apt-get install amulet python3 python3-requests python3-pymongo juju-core charm-tools  -y
